57b4102081e2e3b0ff16d1a0752703f60837789a
Pou18 <grado_daw@hotmail.com>
2024-10-20 09:11:33 +0200

    No se si funcionara

---
+++ Modulo_2/src/main.ts
@@ -0,0 +1,12 @@
+import "./style.css";
+
+console.log({
+    Cena_total : "120€" , 
+    Bebidas: " 18€" , 
+    Amigos: "6" , 
+    Calculo: "(Cena total - Bebidas) dividido entre Amigos"
+});
+
+console.log("Los amigos pagan " + ((120-18)/6) + "€");
+
+console.log("Yo pago las bebidas " + (18) + "€");
