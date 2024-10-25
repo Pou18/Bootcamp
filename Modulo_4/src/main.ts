0ea4d6cf35bd3a7c62ade4d802b470768025cb20
Pou18 <grado_daw@hotmail.com>
2024-10-25 18:58:39 +0200

    Estudiar la función suma

--- Modulo_4/src/main.ts
+++ Modulo_4/src/main.ts
@@ -1,11 +1,12 @@
-function rectangulo(base,altura) {
+function sumar() {
+    // Leer los valores de los inputs
+    const sumando1 = document.getElementById("sumando1").value;
+    const sumando2 = document.getElementById("sumando2").value;
+    // Sumar los dos números
+    const resultado = parseInt(sumando1) + parseInt(sumando2);
+     // Mostrar el resultado
+    document.getElementById("resultado").innerHTML = resultado;
+    }
 
-    return base * altura;
-
-}
-
-console.log(rectangulo(4,4));
-
-
-
-
+const botonSumar = document.getElementById("sumar");
+botonSumar.addEventListener("click", sumar);
