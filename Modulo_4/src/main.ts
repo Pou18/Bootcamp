39640acf1f5fb7a07cafa4bde060d95b5d8534f9
Pou18 <grado_daw@hotmail.com>
2024-10-29 09:55:02 +0100

    Poco a poco

--- Modulo_4/src/main.ts
+++ Modulo_4/src/main.ts
@@ -1,12 +1,5 @@
-function sumar() {
-    // Leer los valores de los inputs
-    const sumando1 = document.getElementById("sumando1").value;
-    const sumando2 = document.getElementById("sumando2").value;
-    // Sumar los dos números
-    const resultado = parseInt(sumando1) + parseInt(sumando2);
-     // Mostrar el resultado
-    document.getElementById("resultado").innerHTML = resultado;
-    }
+const areaDelCuadrado = (lado : number): number => lado * lado;
 
-const botonSumar = document.getElementById("sumar");
-botonSumar.addEventListener("click", sumar);
+
+const area : number = areaDelCuadrado(10);
+console.log(area);
